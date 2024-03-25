e745c79ea3dfd02e8742045654445cc479c0e1cf
mkim425 <minsu@umass.edu>
2024-03-25 15:28:42 -0400

    minor change

--- Rcode/calc-importance-score/importance_score_lomo_trained.R
+++ Rcode/calc-importance-score/importance_score_lomo_trained.R
@@ -1,9 +1,9 @@
 ## Calculate Importance score of a component model using Leave one model out algorithm
 
-# @param forecast_data 'data.frame' containing all the forecasts of 
+# forecast_data 'data.frame' containing all the forecasts of 
 # a certain combination of location, forecast date, and horizon
-# @param truth require data.frame
-# @param ensemble_method method to create quantile ensemble, 
+# truth require data.frame
+# ensemble_method method to create quantile ensemble, 
 # either `median` (default) or `mean`.
 
 # Note: I used 'use_median_as_point = TRUE' in score_forecasts when calculating WIS
